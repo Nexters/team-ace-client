c6ee5bc471314dd012074e75c0b5d0e2707d7887
haeti-dev <t9h9@soongsil.ac.kr>
2025-07-24 00:42:20 +0900

    [chore/#3] 패키지명 변경

--- composeApp/src/commonMain/kotlin/org/nexters/ace/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.nexters.ace
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/iosMain/kotlin/org/nexters/ace/MainViewController.kt
+++
@@ -1,5 +0,0 @@
-package org.nexters.ace
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { App() }
