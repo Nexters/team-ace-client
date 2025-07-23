2cb60bbb95033016f07aa47bda8cdfa5c851b62c
haeti-dev <t9h9@soongsil.ac.kr>
2025-07-23 23:27:12 +0900

    [feature/#3] 확장함수 및 조합용 플러그인 구현

---
+++ build-logic/src/main/java/com/nexters/ace/conventions/primitive/KotlinMultiPlatformIOSPlugin.kt
@@ -0,0 +1,38 @@
+package com.nexters.ace.conventions.primitive
+
+import com.nexters.ace.conventions.Arch
+import com.nexters.ace.conventions.activeArch
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+class KotlinMultiPlatformIOSPlugin : Plugin<Project> {
+    override fun apply(target: Project) = with(target) {
+        extensions.configure<KotlinMultiplatformExtension> {
+            when (activeArch) {
+                Arch.ARM -> {
+                    iosSimulatorArm64()
+                    iosArm64()
+                }
+
+                Arch.ARM_SIMULATOR_DEBUG -> {
+                    iosSimulatorArm64()
+                }
+
+                Arch.X86_64 -> iosX64()
+                Arch.ALL -> {
+                    iosArm64()
+                    iosX64()
+                    iosSimulatorArm64()
+                }
+            }
+            targets.withType<KotlinNativeTarget> {
+                compilations["main"].kotlinOptions.freeCompilerArgs += "-Xexport-kdoc"
+            }
+        }
+    }
+}
