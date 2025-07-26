8f0bb9684599e20d7406ae544f55747addb19cf4
haeti-dev <t9h9@soongsil.ac.kr>
2025-07-26 15:51:24 +0900

    [feature/#8] core:designsystem 모듈 생성

---
+++ core/designsystem/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.ace.kotlin.multiplatform)
+    alias(libs.plugins.ace.compose.multiplatform)
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.bundles.coil)
+        }
+
+    }
+}
+
+compose.resources {
+    publicResClass = true
+}
+
+android.namespace = "com.nexters.ace.core.designsystem"
+
