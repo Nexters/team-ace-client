34461704fe9d3723c8d33a2e8b6f901119c92b22
haeti-dev <t9h9@soongsil.ac.kr>
2025-07-27 16:59:19 +0900

    [feature/#8] feature:main 모듈 생성 - commonMain 모듈에 생성 - 추후 웹 확장성을 위해 nonWasmJs으로 네이밍

---
+++ core/model/fairybook/src/commonMain/kotlin/com/nexters/ace/core/model/fairybook/FairyBook.kt
@@ -0,0 +1,10 @@
+package com.nexters.ace.core.model.fairybook
+
+// 예시 데이터 (추후 변경 가능)
+data class FairyBook(
+    val fairyId: Long,
+    val image: String,
+    val emotion: String,
+    val description: String,
+)
+
