afac42b85f01d8fcf290a63b2db31d8e45cd6eac
hyeseon <konkukbhs@gmail.com>
2025-08-02 14:48:37 +0900

    [feature/#13] firebase 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinxSerialization) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.google.gms) apply false
 }
