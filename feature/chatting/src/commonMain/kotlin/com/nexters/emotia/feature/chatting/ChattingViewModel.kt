908ddcef4560f8ca200f9c9a0d3688d0a0eafcf7
haeti-dev <t9h9@soongsil.ac.kr>
2025-08-04 00:35:45 +0800

    [feature/#9] FeatureModule 생성

---
+++ feature/chatting/src/commonMain/kotlin/com/nexters/emotia/feature/chatting/ChattingViewModel.kt
@@ -0,0 +1,7 @@
+package com.nexters.emotia.feature.chatting
+
+import androidx.lifecycle.ViewModel
+
+class ChattingViewModel : ViewModel() {
+    // TODO: Add chatting logic
+}
---
+++ feature/chatting/src/commonMain/kotlin/com/nexters/emotia/feature/chatting/di/ChattingModule.kt
@@ -0,0 +1,9 @@
+package com.nexters.emotia.feature.chatting.di
+
+import com.nexters.emotia.feature.chatting.ChattingViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val chattingModule = module {
+    viewModel { ChattingViewModel() }
+}
