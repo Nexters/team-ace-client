a6ca38bea458d759ad3b561a6ab3b30fd21fb7fa
hyeseon <konkukbhs@gmail.com>
2025-08-06 00:43:50 +0900

    [feature/#16] 말풍선 구현 - MINE, OTHER로 타입 구분 - 타입에 따른 디자인 대응

---
+++ core/designsystem/src/commonMain/kotlin/com/nexters/emotia/core/designsystem/component/ChatBubble.kt
@@ -0,0 +1,138 @@
+package com.nexters.emotia.core.designsystem.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.nexters.emotia.core.designsystem.theme.EmotiaTheme
+import com.nexters.emotia.core.designsystem.token.LocalEmotiaColors
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+
+enum class BubbleType {
+    MINE,
+    OTHER
+}
+
+data class BubbleStyle(
+    val backgroundColor: Color,
+    val borderColor: Color,
+    val textColor: Color
+)
+
+@Composable
+fun getBubbleStyle(type: BubbleType): BubbleStyle {
+    val colors = LocalEmotiaColors.current
+    return when (type) {
+        // 내가 보낸 말풍선 스타일
+        BubbleType.MINE -> BubbleStyle(
+            backgroundColor = colors.primaryLight,
+            borderColor = colors.primaryDark,
+            textColor = colors.white
+        )
+
+        // 상대방이 보낸 말풍선 스타일
+        BubbleType.OTHER -> BubbleStyle(
+            backgroundColor = colors.black40,
+            borderColor = colors.white40,
+            textColor = colors.white
+        )
+    }
+}
+
+@Composable
+fun ChatBubble(
+    text: String,
+    type: BubbleType,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = TextStyle.Default,
+    cornerRadius: Dp = 8.dp,
+    borderWidth: Dp = 1.dp,
+    padding: Dp = 8.dp,
+    screenHorizontalPadding: Dp = 8.dp,
+    minOppositeMargin: Dp = 60.dp,
+) {
+    val style = getBubbleStyle(type)
+
+    val bubbleModifier = when (type) {
+        BubbleType.MINE -> modifier
+            .fillMaxWidth()
+            .padding(
+                start = minOppositeMargin,
+                end = screenHorizontalPadding
+            )
+
+        BubbleType.OTHER -> modifier
+            .fillMaxWidth()
+            .padding(
+                start = screenHorizontalPadding,
+                end = minOppositeMargin
+            )
+    }
+
+    Box(
+        modifier = bubbleModifier,
+        contentAlignment = when (type) {
+            BubbleType.MINE -> Alignment.CenterEnd
+            BubbleType.OTHER -> Alignment.CenterStart
+        }
+    ) {
+        Surface(
+            shape = RoundedCornerShape(cornerRadius),
+            color = style.backgroundColor,
+            border = BorderStroke(borderWidth, style.borderColor)
+        ) {
+
+            Text(
+                text = text,
+                modifier = Modifier.padding(padding),
+                color = style.textColor,
+                style = textStyle
+            )
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ChatBubblePreview() {
+    EmotiaTheme {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            ChatBubble(
+                text = "졸려",
+                type = BubbleType.OTHER
+            )
+
+            ChatBubble(
+                text = "걱정이 많았어. 특히 장마철에는 습도가 높고 낮 최고기온이 35℃ 이상 지속되기 때문에 음식도 변질되기 쉽고 세균 번식도 활발해지면서 식중독에 의한 장염 발생률이 증가한다.",
+                type = BubbleType.MINE
+            )
+
+            ChatBubble(
+                text = "잠을 잘 자야 해",
+                type = BubbleType.OTHER
+            )
+            ChatBubble(
+                text = "감치왕국 ",
+                type = BubbleType.MINE
+            )
+        }
+    }
+}
+
--- core/designsystem/src/commonMain/kotlin/com/nexters/emotia/core/designsystem/token/Colors.kt
+++ core/designsystem/src/commonMain/kotlin/com/nexters/emotia/core/designsystem/token/Colors.kt
@@ -11,10 +11,13 @@ data class EmotiaColors(
     val darkGray: Color = Color(0xFFACB7B1),
 
     val primaryLight: Color = Color(0xFF45A172),
+    val primaryDark: Color = Color(0xFF21795E),
     val lightGray: Color = Color(0xFF999999),
 
     val white: Color = Color(0xFFFFFFFF),
+    val white40: Color = white.copy(alpha = 0.4f),
     val black: Color = Color(0xFF000000),
+    val black40: Color = black.copy(alpha = 0.4f),
 
     val transparent: Color = Color.Transparent,
 )
