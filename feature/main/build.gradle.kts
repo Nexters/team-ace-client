e5c3c6d6bef7fa16cb48ff2dccebeba82f6d7fb3
haeti-dev <t9h9@soongsil.ac.kr>
2025-08-05 23:15:46 +0800

    [feature/#9] 코드리뷰 반영 및 라이브러리 버전 문제 해결

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -5,15 +5,9 @@ plugins {
 kotlin {
     sourceSets {
         commonMain.dependencies {
-            implementation(projects.core.domain)
-            implementation(projects.core.navigation)
             implementation(projects.feature.onboarding)
             implementation(projects.feature.chatting)
             implementation(projects.feature.result)
-            implementation(libs.koin.core)
-            implementation(libs.koin.compose.viewmodel)
-            implementation(libs.slf4j.api)
-            implementation(libs.slf4j.android)
         }
         val nonWasmJsMain by creating {
             dependsOn(commonMain.get())
---
+++ feature/main/src/commonMain/kotlin/com/nexters/emotia/feature/main/MainNavHost.kt
@@ -0,0 +1,41 @@
+package com.nexters.emotia.feature.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import com.nexters.emotia.feature.chatting.navigation.chattingScreen
+import com.nexters.emotia.feature.onboarding.navigation.onBoardingScreen
+import com.nexters.emotia.feature.result.navigation.resultScreen
+
+@Composable
+internal fun MainNavHost(
+    navigator: MainNavigator,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navigator.navController,
+        startDestination = navigator.startDestination,
+        modifier = modifier
+    ) {
+        onBoardingScreen(
+            onNavigateNext = {
+                navigator.navigateToChatting()
+            }
+        )
+
+        chattingScreen(
+            onNavigateToResult = {
+                navigator.navigateToResult()
+            }
+        )
+
+        resultScreen(
+            onNavigateToOnBoarding = {
+                navigator.navigateToOnBoarding()
+            },
+            onNavigateToChatting = {
+                navigator.navigateToChatting()
+            }
+        )
+    }
+}
