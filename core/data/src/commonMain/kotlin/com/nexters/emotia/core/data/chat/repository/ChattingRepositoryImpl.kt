4bf7030b592207ff4910e4a3cc1fef71bf038688
hyeseon <konkukbhs@gmail.com>
2025-08-08 00:45:14 +0900

    [feat/#18] 채팅 API data 모듈 구성

--- core/data/src/commonMain/kotlin/com/nexters/emotia/core/data/chat/repository/ChattingRepositoryImpl.kt
+++ core/data/src/commonMain/kotlin/com/nexters/emotia/core/data/chat/repository/ChattingRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.nexters.emotia.core.data.chat.repository
 import com.nexters.emotia.core.data.chat.datasource.ChattingRemoteDataSource
 import com.nexters.emotia.core.data.chat.mapper.toDomain
 import com.nexters.emotia.domain.chat.ChattingRepository
+import com.nexters.emotia.domain.chat.entity.ChatMessage
 import com.nexters.emotia.domain.chat.entity.ChattingRoom
 
 class ChattingRepositoryImpl(
@@ -20,6 +21,20 @@ class ChattingRepositoryImpl(
             response.toDomain()
         }.onFailure { exception ->
             println("채팅룸 생성 실패: ${exception.message}")
+        }
+    }
+
+    override suspend fun sendChat(roomId: Int, message: String): Result<ChatMessage> {
+        return runCatching {
+            val response = remoteDataSource.sendChat(roomId, message)
+
+            if (!response.success) {
+                throw IllegalStateException("API 요청 실패: ${response.error}")
+            }
+
+            response.toDomain()
+        }.onFailure { exception ->
+            println("채팅 전송 실패: ${exception.message}")
         }
     }
 }
