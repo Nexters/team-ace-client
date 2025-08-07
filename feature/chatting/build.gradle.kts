90c0558b49cc2924f8f03d972c21ae400d396da1
hyeseon <konkukbhs@gmail.com>
2025-08-08 00:23:32 +0900

    [feat/#18] 채팅방 생성 Screen 구현

--- feature/chatting/build.gradle.kts
+++ feature/chatting/build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
 kotlin {
     sourceSets {
         commonMain.dependencies {
+            implementation(projects.core.domain)
+            implementation(projects.core.designsystem)
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose.viewmodel)
+            implementation(libs.slf4j.api)
+            implementation(libs.slf4j.android)
         }
     }
 }
