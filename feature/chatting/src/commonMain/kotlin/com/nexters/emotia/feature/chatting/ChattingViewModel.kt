e1ea4f36594bbed8cd5ef88ac449060cc84b57c4
hyeseon <konkukbhs@gmail.com>
2025-08-08 00:56:17 +0900

    [feat/#18] 채팅 API 연결해 뷰모델 구성

--- feature/chatting/src/commonMain/kotlin/com/nexters/emotia/feature/chatting/ChattingViewModel.kt
+++ feature/chatting/src/commonMain/kotlin/com/nexters/emotia/feature/chatting/ChattingViewModel.kt
@@ -81,40 +81,43 @@ class ChattingViewModel(
             type = BubbleType.MINE
         )
 
+        val messageToSend = uiState.currentInputText
+        val roomId = uiState.roomId!!
+
         uiState = uiState.copy(
             messages = uiState.messages + userMessage,
             currentInputText = "",
             isLoading = true
         )
 
-        // 지금은 임시 응답으로 처리
-        simulateAIResponse()
+        sendChatMessage(roomId, messageToSend)
     }
 
 
-    private fun simulateAIResponse() {
+    private fun sendChatMessage(roomId: Int, message: String) {
         viewModelScope.launch {
             // chatting typing indicator 를 보기 위해 딜레이 걸어둠
             kotlinx.coroutines.delay(1000)
 
-            val responses = listOf(
-                "그렇게 느끼시는군요. 더 자세히 말씀해주세요.",
-                "이해합니다. 그런 감정이 드는 이유가 있을까요?",
-                "흥미로운 이야기네요. 어떤 기분이신가요?",
-                "힘든 하루를 보냈다니 안타깝네요. 어떤 일이 있었나요?",
-                "그런 기분이 드셨군요. 어떤 상황에서 그런 감정을 느꼈나요?",
-                "그런 감정을 느끼셨군요. 어떤 일이 있었나요?"
-            )
+            chattingRepository.sendChat(roomId, message)
+                .onSuccess { chatResponse ->
+                    val aiResponse = ChatMessage(
+                        text = chatResponse.message,
+                        type = BubbleType.OTHER
+                    )
 
-            val aiResponse = ChatMessage(
-                text = responses.random(),
-                type = BubbleType.OTHER
-            )
-
-            uiState = uiState.copy(
-                messages = uiState.messages + aiResponse,
-                isLoading = false
-            )
+                    uiState = uiState.copy(
+                        messages = uiState.messages + aiResponse,
+                        isLoading = false,
+                        error = null
+                    )
+                }
+                .onFailure { exception ->
+                    uiState = uiState.copy(
+                        isLoading = false,
+                        error = "메시지 전송에 실패했습니다: ${exception.message}"
+                    )
+                }
         }
     }
 
