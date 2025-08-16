a77e3f3fe0cd5af58681cca538f512f519ad7148
최지철 <sksk02zja@gmail.com>
2025-08-16 21:29:16 +0900

    [Feat/#23] 싱글톤 객체 의존성 추가

--- core/data/onboarding/src/commonMain/kotlin/com/nexters/emotia/core/data/onboarding/di/DataOnboardingModule.kt
+++ core/data/onboarding/src/commonMain/kotlin/com/nexters/emotia/core/data/onboarding/di/DataOnboardingModule.kt
@@ -3,7 +3,9 @@ package com.nexters.emotia.core.data.onboarding.di
 import com.nexters.emotia.core.data.onboarding.datasource.LoginDataSource
 import com.nexters.emotia.core.data.onboarding.datasource.LoginDataSourceImpl
 import com.nexters.emotia.core.data.onboarding.repository.LoginRepositoryImpl
+import com.nexters.emotia.core.data.onboarding.repository.TokenRepositoryImpl
 import com.nexters.emotia.core.domain.onboarding.LoginRepository
+import com.nexters.emotia.core.domain.onboarding.repository.TokenRepository
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -15,4 +17,5 @@ val dataOnboardingModule =
 
         // Repository 주입
         singleOf(::LoginRepositoryImpl).bind<LoginRepository>()
+        singleOf(::TokenRepositoryImpl).bind<TokenRepository>()
     }
