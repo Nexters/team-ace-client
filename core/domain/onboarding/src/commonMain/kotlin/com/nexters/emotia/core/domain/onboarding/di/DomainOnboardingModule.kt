91a21af5e8ec8df1a0d49a77e815823e23421969
최지철 <sksk02zja@gmail.com>
2025-08-16 21:29:10 +0900

    [Feat/#23] 싱글톤 객체 의존성 추가

--- core/domain/onboarding/src/commonMain/kotlin/com/nexters/emotia/core/domain/onboarding/di/DomainOnboardingModule.kt
+++ core/domain/onboarding/src/commonMain/kotlin/com/nexters/emotia/core/domain/onboarding/di/DomainOnboardingModule.kt
@@ -1,9 +1,13 @@
 package com.nexters.emotia.core.domain.onboarding.di
 
+import com.nexters.emotia.core.domain.onboarding.manager.TokenManager
 import com.nexters.emotia.core.domain.onboarding.usecase.AutoLoginUseCase
+import com.nexters.emotia.core.domain.onboarding.usecase.TokenUseCase
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val domainOnboardingModule = module {
+    single { TokenManager() }
     singleOf(::AutoLoginUseCase)
+    singleOf(::TokenUseCase)
 }
