76b889dec3989dd44bc719ff9bb54a486e00d3e0
hyeseon <konkukbhs@gmail.com>
2025-08-22 22:07:56 +0900

    [feature/#42] splashScreen 구현
    - 아이콘 네모 현상 수정 필요

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -23,6 +23,8 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.core.ktx)
+            implementation(libs.androidx.core.splashscreen)
             implementation(project.dependencies.platform(libs.google.firebase.bom))
         }
 
