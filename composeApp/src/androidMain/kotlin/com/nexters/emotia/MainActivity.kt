9743db3b5e4f5cb3d088cff2fac9da6a74812432
hyeseon <konkukbhs@gmail.com>
2025-09-21 00:20:25 +0900

    [feature/#44] 상태바 아이콘 색상 반전

--- composeApp/src/androidMain/kotlin/com/nexters/emotia/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/nexters/emotia/MainActivity.kt
@@ -7,14 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.nexters.emotia.core.platform.AndroidContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
-        
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
+        // 상태 바 아이콘 색상 반전
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars =
+            false
 
         // Android Context 초기화
         AndroidContext.init(this)
